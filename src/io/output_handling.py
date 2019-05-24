958a9178b6f9bba6208e392eaccd9eb8c26da202
Anne Sallaska <annes@porch.com>
2019-05-23 21:50:22 -0700

    making steps newlines

--- src/io/output_handling.py
+++ src/io/output_handling.py
@@ -15,8 +15,11 @@ def extract_team_scores(team_class_list: List[t.Team], output_file_path: str) ->
     """
     scored_df = pd.DataFrame([[team.team_name, team.total_score] for team in team_class_list],
                              columns=["Team", "Total Score"])
-    scored_and_ranked_df = scored_df.assign(Rank=lambda x: x["Total Score"].rank(ascending=False, method="max")) \
+    
+    scored_and_ranked_df = scored_df \
+        .assign(Rank=lambda x: x["Total Score"].rank(ascending=False, method="max")) \
         .sort_values(by="Rank")
+
     scored_and_ranked_df.to_csv(output_file_path)
 
     return None
